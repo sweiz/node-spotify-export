95dc118f1bc56b07c24ff37e7d0f85956c07a21e
sweiz <sam.jury@gmail.com>
2014-11-01 04:09:49 +1100

    Added `all` option to process all playlists in a directory. Fixed relative directory issues

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/node
 /* jshint
       laxcomma  :   true
     , laxbreak  :   true
@@ -11,38 +12,57 @@ var path = require('path')
   , request = require('request')
   , Handlebars = require('handlebars')
   , progressBar = require('progress')
+  , cwd = process.cwd()
   , file = process.argv[2]
   , format = process.argv[3] || 'xspf'
+  , paths = {
+      file: path.normalize(cwd+'/'+file)
+    , output: path.normalize(cwd+'/'+file+'.'+format)
+  }
   , albumImages = []
-  , tracks = []
   , config = require('./config')
   , tmpl = fs.existsSync(path.normalize(__dirname+'/templates/'+format+'.hbs')) ? Handlebars.compile(fs.readFileSync(path.normalize(__dirname+'/templates/'+format+'.hbs'), 'utf8')) : false
-;
-if(!tmpl || !fs.existsSync(path.normalize(__dirname+'/'+file))) {
-  console.log('Error: Missing Templates and/or cannot file file');
-  console.log('TemplateDir: '+path.normalize(__dirname+'/templates/'+format+'.hbs'));
-  console.log('FileDir: '+path.normalize(__dirname+'/'+file));
-}
-else {
-  fs.readFile(path.normalize(__dirname+'/'+file), 'utf8', function(err,fileContents){
-    if(err) throw err;
-    console.log('Tracklist loaded. Starting conversion...');
-    var tracksList = fileContents.split(' ')
-      , progress = new progressBar(':bar', {width: 50,total: tracksList.length+1});
-    async.eachSeries(tracksList, function(URI, cb){
-      request(config.track(URI), function(err, status, body){
-        if(err) cb(err);
-        var track = JSON.parse(body);
-        tracks.push(track);
-        progress.tick();
-        cb(null);
-      });
-    }, function(err){
+  , convert = function(file, cb){
+    var thisFile = path.normalize(path.join(cwd, file));
+    fs.readFile(thisFile, 'utf8', function(err,fileContents){
       if(err) throw err;
-      fs.writeFile(path.normalize(__dirname+'/'+file+'.'+format), tmpl({tracks:tracks}), function(err){
-        if(err) throw err;
-        console.log('\nDone: '+path.normalize(__dirname+'/'+file+'.'+format));
+      console.log('\n Converting: '+file);
+      var trackURIs = fileContents.split(' ')
+        , progress = new progressBar(':bar', {width: 50,total: trackURIs.length+1});
+      async.map(trackURIs, function(URI, cb){
+        request(config.track(URI), function(err, status, body){
+          if(err) return cb(err);
+          var track;
+          try{
+            track = JSON.parse(body);
+          } catch(err){
+            return cb(err);
+          }
+          cb(null, track);
+        });
+      }, function(err, tracks){
+          if(err) throw err;
+          fs.writeFile(thisFile+'.'+format, tmpl({tracks:tracks}), function(err){
+            if(err) {
+              if(cb) return cb(err);
+              else throw err;
+            }
+            if(cb) return cb(null);
+            console.log('\nDone: '+paths.output);
+          });
       });
     });
+  }
+;
+if(!tmpl) { return console.log('Error: Missing Templates\nTemplateDir: '+path.normalize(__dirname+'/templates/'+format+'.hbs'));}
+if(file === 'all' || file === 'ALL' || file === "All"){
+  fs.readdir(cwd, function(err, files){
+    async.each(files, function(thisFile, cb){
+      convert(thisFile, cb);
+    }, function(err){
+      console.log('\nDone!');
+    });
   });
+} else {
+  convert(file);
 }
